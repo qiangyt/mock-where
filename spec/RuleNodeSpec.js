6ccfa072ddeead788827a68ee4b881e8a65cc008
Qiang Yiting <qiangyt@wxcount.com>
2017-08-07 22:50:39 +0800

    Improve RuleNode unit test

--- spec/RuleNodeSpec.js
+++ spec/RuleNodeSpec.js
@@ -3,7 +3,7 @@ const RuleNode = require('../src/RuleNode');
 describe("RuleNode test suite: ", function() {
     it("put the rule on this node exactly", function() {
         const n = new RuleNode('/', '/');
-        const rule = {};
+        const rule = { A: 'A' };
 
         expect(n.put('/', 0, rule)).toBe(true);
         expect(n.children.length).toBe(0);
@@ -13,7 +13,7 @@ describe("RuleNode test suite: ", function() {
 
     it("put the rule on new child node", function() {
         const n = new RuleNode('/', '/');
-        const rule = {};
+        const rule = { A: 'A' };
 
         expect(n.put('/1', 0, rule)).toBe(true);
         expect(n.children.length).toBe(1);
@@ -26,10 +26,10 @@ describe("RuleNode test suite: ", function() {
 
     it("put the rule on existing child node", function() {
         const n = new RuleNode('/', '/');
-        const rule1 = {};
-        expect(n.put('/1', 0, rule1)).toBe(true);
+        const rule1 = { A: 'A' };
+        n.put('/1', 0, rule1);
 
-        const rule2 = {};
+        const rule2 = { B: 'B' };
 
         expect(n.put('/1', 0, rule2)).toBe(true);
         expect(n.children.length).toBe(1);
@@ -40,9 +40,33 @@ describe("RuleNode test suite: ", function() {
         expect(child.rules[1]).toEqual(rule2);
     });
 
+    it("put the rule on secondary existing child node", function() {
+        const n = new RuleNode('/', '/');
+
+        const rule1 = { A: 'A' };
+        n.put('/1', 0, rule1);
+
+        const rule2 = { B: 'B' };
+        n.put('/2', 0, rule2);
+
+        const rule3 = { C: 'C' };
+
+        expect(n.put('/2', 0, rule3)).toBe(true);
+        expect(n.children.length).toBe(2);
+        expect(n.rules.length).toBe(0);
+
+        const child1 = n.children[0];
+        expect(child1.rules.length).toBe(1);
+        expect(child1.rules[0]).not.toEqual(rule3);
+
+        const child2 = n.children[1];
+        expect(child2.rules.length).toBe(2);
+        expect(child2.rules[1]).toEqual(rule3);
+    });
+
     it("put rule with different path letter", function() {
         const n = new RuleNode('/', '/');
-        const rule = {};
+        const rule = { A: 'A' };
 
         expect(n.put('a', 0, rule)).toBe(false);
     });
