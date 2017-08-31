9096b29e9ee7d49d530115b82e7c3bd7e41dfdc2
Qiang Yiting <qiangyt@wxcount.com>
2017-08-31 08:23:22 +0800

    init doc for api/Where, should be switched to md

--- src/api/Where.js
+++ src/api/Where.js
@@ -2,6 +2,37 @@ const MissingParamError = require('qnode-error').MissingParamError;
 const RequestError = require('qnode-error').RequestError;
 
 
+/**
+ * API: add a rule.
+ * 
+ * The rule should be in request body. 
+ * Example: {
+ *      port:  8000,
+ *      domain:  "github.com",
+ *      name: "My rule name", // optional. assigned by default
+ *      path: "/test", // optional. assigned to default path, if not specified
+ *      method: "POST", // optional. assigned to default method, if not specified
+ *      q: "ip='localhost'", // optional. assigned to default query, if not specified
+ *      delay: 200, // optoinal. assigned to default delay, if not specified
+ *      delayFix: 16, // optional. assigned to default delayFix, if not specified
+ *      response:  // optional. merged with default response
+ *      {
+ *          status: 403,
+ *          type: "application/json",
+ *          template:  // exclusive with "body"
+ *          {
+ *              type: "mustache", // optional. if not specified, assigned to "ejs" 
+ *                                // if template is text, otherwise to default response.templateType
+ *              text: "hi", // optional. if not specified, assigned to "template text not specified"
+ *          },
+ *          body: // exclusive with "template".
+ *                // could be any JSON-stringify-able object, string, or primitive value,
+ *                // "no response body specified", if not specified
+ *          {
+ *          }
+ *      }
+ * }
+ */
 class Where {
 
     init() {
@@ -16,7 +47,7 @@ class Where {
 
         const domain = rule.domain;
         if (!domain) throw new MissingParamError('domain');
-        
+
         const port = rule.port;
         if (!port) throw new MissingParamError('port');
 
