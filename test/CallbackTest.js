d1ab42d51921e8af9fc50946cf191acf6f020dc9
Qiang Yiting <qiangyt@wxcount.com>
2017-09-01 07:52:03 +0800

    Test Callback.normalizeAsyncFlag

--- test/CallbackTest.js
+++ test/CallbackTest.js
@@ -5,8 +5,11 @@ const Callback = require(`${SRC}/Callback`);
 
 describe("Callback test suite: ", function() {
 
-    it(": ", function() {
-
+    it("normalizeAsyncFlag(): happy", function() {
+        expect(Callback.normalizeAsyncFlag()).toBe(false);
+        expect(Callback.normalizeAsyncFlag(true)).toBe(true);
+        expect(Callback.normalizeAsyncFlag(false)).toBe(false);
+        expect(Callback.normalizeAsyncFlag(null)).toBe(false);
     });
 
 });
