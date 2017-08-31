2ff4edb98b652c510e16253dc54ec58677ec54ce
Qiang Yiting <qiangyt@wxcount.com>
2017-09-01 07:57:54 +0800

    test Callback.normalizeList()

--- test/CallbackTest.js
+++ test/CallbackTest.js
@@ -12,4 +12,17 @@ describe("Callback test suite: ", function() {
         expect(Callback.normalizeAsyncFlag(null)).toBe(false);
     });
 
+    it("normalizeList(): happy", function() {
+        expect(Callback.normalizeList(undefined).length).toBe(0);
+        expect(Callback.normalizeList(null).length).toBe(0);
+        expect(Callback.normalizeList([]).length).toBe(0);
+
+        expect(Callback.normalizeList([{}, {}]).length).toBe(2);
+    });
+
+    it("normalizeTarget(): method", function() {
+        expect(Callback.normalizeTarget({}).method).toBe('post');
+        expect(Callback.normalizeTarget({ method: 'get' }).method).toBe('get');
+    });
+
 });
